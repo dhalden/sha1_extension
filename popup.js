4575384097e88f4208273f7a5d97a74a30716239
dhalden <dhalden@github.com>
2015-08-30 17:54:54 -0600

    add bit padding

--- popup.js
+++ popup.js
@@ -14,14 +14,15 @@
 var hexcase = 0; /* hex output format. 0 - lowercase; 1 - uppercase */
 var b64pad = "="; /* base-64 pad character. "=" for strict RFC compliance */
 var chrsz = 8; /* bits per input character. 8 - ASCII; 16 - Unicode */
+var blocksize = 64;
 
 /*
  * * These are the functions you'll usually want to call
  * * They take string arguments and return either hex or base-64 encoded strings
  * */
-function hex_sha1(s){return binb2hex(core_sha1(str2binb(s),s.length * chrsz));}
-function b64_sha1(s){return binb2b64(core_sha1(str2binb(s),s.length * chrsz));}
-function brand_sha1(s){return bin2brand(core_sha1(str2binb(s),s.length * chrsz));}
+function hex_sha1(x,y){return binb2hex(hmac_sha1(x,y));}
+function b64_sha1(x,y){return binb2b64(hmac_sha1(x,y));}
+function b96_sha1(x,y){return binb2b96(hmac_sha1(x,y));}
 
 /*
  * * Calculate the SHA-1 of an array of big-endian words, and a bit length
@@ -159,9 +160,10 @@ function binb2b64(binarray)
     return str;
 }
 
-function bin2brand(binarray)
+function binb2b96(binarray)
 {
     s = document.getElementById('message').value;
+    s += document.getElementById('prefix').value;
     tab = make_st(core_sha1(str2binb(s),s.length * chrsz));
     var str = "";
     for(var i = 0; i < binarray.length * 4; i += 3)
@@ -180,13 +182,15 @@ function bin2brand(binarray)
 }
 
 function make_st(binarray) {
-    var tab  = "!#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+    var tab  = "!#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\" +
+               "]^_`abcdefghijklmnopqrstuvwxyz{|}~";
     var i = 0;
     while(tab.length > 64) {
         var triplet = (((binarray[i >> 2] >> 8 * (3 - i %4)) & 0xFF) << 16)
             | (((binarray[i+1 >> 2] >> 8 * (3 - (i+1)%4)) & 0xFF) << 8 )
             | ((binarray[i+2 >> 2] >> 8 * (3 - (i+2)%4)) & 0xFF);
-        tab = tab.slice(0, (triplet % tab.length)) + tab.slice((triplet % tab.length) + 1, tab.length);
+        tab = tab.slice(0, (triplet % tab.length)) +
+              tab.slice((triplet % tab.length) + 1, tab.length);
         i++;
     }
     return tab;
@@ -198,24 +202,80 @@ function limitlength(val) {
     return val.slice(0,limit);
 }
 
+function hmac_sha1(key, message) {
+    if(key.length > blocksize) {
+        key = binb2hex(core_sha1(key, key.length * chrsz));
+    } else if (key.length < blocksize) {
+        key = pad(key, (blocksize - key.length));
+    }
+    var o_key_pad = [];
+    var i_key_pad = [];
+    var o_pad = pad(0x5c, blocksize);
+    var i_pad = pad(0x36, blocksize);
+
+    for(i=0; i < key.length; i++) {
+        o_key_pad.push(key.charCodeAt(i) ^ o_pad.charCodeAt(i))
+    }
+    o_key_pad = bina(o_key_pad);
+
+    for(i=0; i < key.length; i++) {
+        i_key_pad.push(key.charCodeAt(i) ^ i_pad.charCodeAt(i))
+    }
+    i_key_pad = bina(i_key_pad);
+    var ipm = [];
+    var opi = []; 
+
+    message = pad(message, i_key_pad.length)
+    for(i=0; i < message.length; i++) {
+        ipm.push(message.charCodeAt(i) | i_key_pad.charCodeAt(i))
+    }
+    ipm = bina(ipm);
+    
+    var cs1im = binb2hex(core_sha1(ipm, ipm.length * chrsz));
+    cs1im = pad(cs1im, o_key_pad.length)
+    for(i=0; i < message.length; i++) {
+        opi.push(cs1im.charCodeAt(i) | o_key_pad.charCodeAt(i))
+    }
+    opi = bina(opi);
+    return core_sha1(opi, opi.length * chrsz);
+}
+
+function bina(binarray)
+{
+    var hex_tab = hexcase ? "0123456789ABCDEF" : "0123456789abcdef";
+    var str = "";
+    for(var i = 0; i < binarray.length * 4; i++)
+    {
+        str += hex_tab.charAt(binarray[i] % hex_tab.length); 
+    }
+    return str;
+}
+
+function pad(n, width, z) {
+      z = z || '0';
+        n = n + '';
+          return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     var hash = document.getElementById('hashb');
     hash.addEventListener('click', function() {
-        document.getElementById('hash').value = limitlength(hex_sha1(document.getElementById('prefix').value + document.getElementById('message').value));
+        document.getElementById('hash').value =
+        limitlength(hex_sha1(document.getElementById('prefix').value, 
+                  document.getElementById('message').value));
+    });
+
+    var hashb = document.getElementById('b64b');
+    hashb.addEventListener('click', function() {
+        document.getElementById('hash').value =
+        limitlength(b64_sha1(document.getElementById('prefix').value, 
+                document.getElementById('message').value));
+    });
+
+    var hashr = document.getElementById('random');
+    hashr.addEventListener('click', function() {
+        document.getElementById('hash').value =
+        limitlength(b96_sha1(document.getElementById('prefix').value,
+                document.getElementById('message').value));
     });
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-    var hash = document.getElementById('b64b');
-    hash.addEventListener('click', function() {
-        document.getElementById('hash').value = limitlength(b64_sha1(document.getElementById('prefix').value + document.getElementById('message').value));
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    var hash = document.getElementById('random');
-    hash.addEventListener('click', function() {
-        document.getElementById('hash').value = limitlength(brand_sha1(document.getElementById('prefix').value + document.getElementById('message').value));
-    });
-});
-
